1f95204d11032d80c5ee9c2ac22cf91ef093b681
Christopher Carson <chris@nowzoo.com>
2018-06-08 14:40:03 -0500

    docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {
-
-  }
-
-  ngOnInit() {
-    this.router.events.pipe(filter(e => e instanceof ActivationEnd)).subscribe(() => {
-      window.scroll(0, 0);
-    });
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgxHighlightJsModule, NgxHighlightJsOptions } from '@nowzoo/ngx-highlight-js';
 import { AppComponent } from './app.component';
-import { ExamplesRouteComponent } from './examples-route/examples-route.component';
 import { ExampleUrlComponent } from './example-url/example-url.component';
 import { ExampleUrlVariableComponent } from './example-url-variable/example-url-variable.component';
 import { ExampleStringComponent } from './example-string/example-string.component';
 import { ExampleStringVariableComponent } from './example-string-variable/example-string-variable.component';
 import { ExampleSetThemeComponent } from './example-set-theme/example-set-theme.component';
 
-const routes: Routes = [
-  {path: '', component: ExamplesRouteComponent},
-];
 const opts = new NgxHighlightJsOptions();
 opts.theme = 'monokai-sublime';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExamplesRouteComponent,
     ExampleUrlComponent,
     ExampleUrlVariableComponent,
     ExampleStringComponent,
@@ -30,7 +24,6 @@ opts.theme = 'monokai-sublime';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes),
     NgxHighlightJsModule.forRoot()
   ],
   providers: [{provide: NgxHighlightJsOptions, useValue: opts}],
